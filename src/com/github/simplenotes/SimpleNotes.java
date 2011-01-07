a66c5236171cad3e56b306bc1d929bee0559e4db
Mattias Svala <thebrasse@brasse.org>
2011-01-07 12:43:09 +0100

    Adding a menu.

--- src/com/github/simplenotes/SimpleNotes.java
+++ src/com/github/simplenotes/SimpleNotes.java
@@ -2,12 +2,38 @@ package com.github.simplenotes;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class SimpleNotes extends ListActivity {
+
+	public static final int ADD_NOTE_ID = Menu.FIRST;
+	public static final int SETTINGS_ID = Menu.FIRST + 1;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notelist);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        boolean result = super.onCreateOptionsMenu(menu);
+        menu.add(0, ADD_NOTE_ID, 0, R.string.add_note);
+        menu.add(0, SETTINGS_ID, 0, R.string.settings);
+        return result;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case ADD_NOTE_ID:
+            return true;
+        case SETTINGS_ID:
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
